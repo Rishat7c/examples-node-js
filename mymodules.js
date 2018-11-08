0ca5ed49ac848c804ea3c8e747051f66d965f4cf
Rishat <xpan96@gmail.com>
2018-11-08 19:57:35 +0300

    optim

--- mymodules.js
+++ mymodules.js
@@ -1,19 +1,14 @@
 // Func
-var arResult = function (array) {
+module.exports.arResult = function (array) {
     return "В массиве находится " + array.length + " элементов"
 };
 
 // Param
-var multiply = function (x, y) {
+module.exports.multiply = function (x, y) {
   return `${x} умножить ${y} равно ${x * y}`;
 }
 
 
-var value = 6;
+module.exports.value = 6;
 // Print
 // console.log(arResult([0, 7, 14, 21, 28, 35]));
-
-// Export
-module.exports.arResult = arResult;
-module.exports.multiply = multiply;
-module.exports.value = value;
