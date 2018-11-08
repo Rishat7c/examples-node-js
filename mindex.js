1056f33053a4a2eda44053449861854191386719
Rishat <xpan96@gmail.com>
2018-11-08 20:36:46 +0300

    base

--- mindex.js
+++ mindex.js
@@ -1,7 +1,8 @@
-var mymodules = require('./mymodules'); // Подключения модуля без его расширения
+// var mymodules = require('./mymodules'); // Подключения модуля без его расширения
+// console.log(mymodules.value);
+// console.log(mymodules.arResult([0, 7, 14, 21, 28, 35, 42]));
+// console.log(mymodules.multiply(5, 3));
 
-console.log(mymodules.value);
+var events = require('events');
 
-console.log(mymodules.arResult([0, 7, 14, 21, 28, 35, 42]));
-
-console.log(mymodules.multiply(5, 3));
+var myEmit;
