8418853a98e5b6c0b9d34db241ca0be13d275c25
Rishat <xpan96@gmail.com>
2018-11-08 19:59:38 +0300

    arrays export

--- mymodules.js
+++ mymodules.js
@@ -1,14 +1,21 @@
 // Func
-module.exports.arResult = function (array) {
+var arResult = function (array) {
     return "В массиве находится " + array.length + " элементов"
 };
 
 // Param
-module.exports.multiply = function (x, y) {
+var multiply = function (x, y) {
   return `${x} умножить ${y} равно ${x * y}`;
 }
 
 
-module.exports.value = 6;
+var value = 6;
 // Print
 // console.log(arResult([0, 7, 14, 21, 28, 35]));
+
+// Export
+module.exports = {
+    arResult: arResult,
+    multiply: multiply,
+    value: value
+};
