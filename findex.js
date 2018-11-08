35b74fdb59504c0bbffc18db4f5a636eb3f178e3
Rishat <xpan96@gmail.com>
2018-11-08 19:29:01 +0300

    re-save

---
+++ findex.js
@@ -0,0 +1,15 @@
+// Функции
+function sayHey() {
+  console.log("Hey");
+}
+
+// Каллирование функции
+sayHey();
+
+// Функция перменная
+var printSomething = function() {
+  console.log("text...");
+}
+
+// Калирование функции
+printSomething();
