775aad4c6920a38de8bbaeb7793c1579174cb20c
Rishat <xpan96@gmail.com>
2018-11-08 19:51:09 +0300

    initial

--- mymodules.js
+++ mymodules.js
@@ -3,8 +3,15 @@ var arResult = function (array) {
     return "В массиве находится " + array.length + " элементов"
 };
 
+// Param
+var multiply = function (x, y) {
+  return `$(x) умножить $(y) равно $(x * y)`;
+}
+
+
+var value = 6;
 // Print
 // console.log(arResult([0, 7, 14, 21, 28, 35]));
 
 // Export
-module.exports = arResult;
+module.exports = arResult, value;
