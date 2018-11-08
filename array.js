b64eb729acaf415c15a130b2649943067048e03a
Rishat <xpan96@gmail.com>
2018-11-08 19:41:09 +0300

    export module and param

--- array.js
+++ array.js
@@ -4,4 +4,7 @@ var arResult = function (array) {
 };
 
 // Print
-console.log(arResult([0, 7, 14, 21, 28, 35]));
+// console.log(arResult([0, 7, 14, 21, 28, 35]));
+
+// Export
+module.exports = arResult;
