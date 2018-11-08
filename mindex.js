2050838f29d2902836ab205e3d09ec54142766e1
Rishat <xpan96@gmail.com>
2018-11-08 19:54:28 +0300

    call

--- mindex.js
+++ mindex.js
@@ -1,3 +1,7 @@
-var array = require('./mymodules'); // Подключения модуля без его расширения
+var mymodules = require('./mymodules'); // Подключения модуля без его расширения
 
-console.log(array([0, 7, 14, 21, 28, 35, 42]));
+console.log(mymodules.value);
+
+console.log(mymodules.arResult([0, 7, 14, 21, 28, 35, 42]));
+
+console.log(mymodules.multiply(5, 3));
