4663b1e238cb13092f0a4df31ed78fee202ea218
Rishat <xpan96@gmail.com>
2018-11-09 20:01:05 +0300

    async

--- tindex.js
+++ tindex.js
@@ -1,11 +1,16 @@
 var fs = require('fs');
 
+/**
+* Синхронное чтение и запись
+*/
 // Чтение файла
 var fileReader = fs.readFileSync('./text.txt', 'utf8');
-
 // Код ниже не будет вызван, пока объект fs не прочтет весь файл
 console.log(fileReader);
-
 // Запись в файл
 var message = fileReader + "\nПривет\nВсе круто!"
 var writeReader = fs.writeFileSync('./text.txt', message);
+
+/**
+* Асинхронное чтение и запись
+*/
