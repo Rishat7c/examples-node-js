f7e6ebc5ff3b54b6e0fdda5664da1f5b32cdf49f
Rishat <xpan96@gmail.com>
2018-11-09 19:56:19 +0300

    launch tindex.js

--- tindex.js
+++ tindex.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 
 // Чтение файла
-fs.readFileSync();
+var fileReader = fs.readFileSync('./text.txt', 'utf8');
 
 // Код ниже не будет вызван, пока объект fs не прочтет весь файл
+console.log(fileReader);
