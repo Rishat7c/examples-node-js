25434ec910ae70afffc120cde88c5053130bd06a
Rishat <xpan96@gmail.com>
2018-11-09 19:59:37 +0300

    write in txt file

--- tindex.js
+++ tindex.js
@@ -5,3 +5,7 @@ var fileReader = fs.readFileSync('./text.txt', 'utf8');
 
 // Код ниже не будет вызван, пока объект fs не прочтет весь файл
 console.log(fileReader);
+
+// Запись в файл
+var message = fileReader + "\nПривет\nВсе круто!"
+var writeReader = fs.writeFileSync('./text.txt', message);
