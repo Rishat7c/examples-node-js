c0e624bd453ac55794c9757e277599e34f90bda2
Rishat <xpan96@gmail.com>
2018-11-11 22:04:59 +0300

    del dir & files in dir

--- dindex.js
+++ dindex.js
@@ -6,13 +6,15 @@ var fs = require('fs');
 // Create dir & sync
 // fs.mkdirSync('upload', function() {});
 // Create dir & async
-fs.mkdir('upload', function() {
-    fs.writeFile('./upload/index.js', '', function() {
-        console.log('done!');
-    });
-});
+// fs.mkdir('upload', function() {
+//     fs.writeFile('./upload/index.js', '', function() {
+//         console.log('done!');
+//     });
+// });
 
 // Remove dir & sync
 // fs.rmdirSync('upload', function() {});
+// п/с/ фича следующая, что перед удалением директа, нужна очистить его
+fs.unlink('./upload/index.js', function() {});
 // Remove dir & async
-// fs.rmdir('upload', function() {});
+fs.rmdir('upload', function() {});
