3ee989630b04aea6135d08ef9c686f3387516d1a
Rishat <xpan96@gmail.com>
2018-11-11 21:50:50 +0300

    create & remove dir

--- dindex.js
+++ dindex.js
@@ -1,4 +1,10 @@
 var fs = require('fs');
 
 // Delete file - sync
-fs.unlink('dtext.txt', function() {}); 
+// fs.unlink('dtext.txt', function() {});
+
+// Create dir & sync
+fs.mkdirSync('upload', function() {});
+
+// Remove dir & sync
+fs.rmdir('upload', function() {});
