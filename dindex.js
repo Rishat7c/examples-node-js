69ae7ba62eaf023b2ecbf18b0d475dd3d4d6e056
Rishat <xpan96@gmail.com>
2018-11-11 21:54:04 +0300

    async create dir & remove

--- dindex.js
+++ dindex.js
@@ -4,7 +4,11 @@ var fs = require('fs');
 // fs.unlink('dtext.txt', function() {});
 
 // Create dir & sync
-fs.mkdirSync('upload', function() {});
+// fs.mkdirSync('upload', function() {});
+// Create dir & async
+fs.mkdir('upload', function() {});
 
 // Remove dir & sync
+// fs.rmdirSync('upload', function() {});
+// Remove dir & async
 fs.rmdir('upload', function() {});
