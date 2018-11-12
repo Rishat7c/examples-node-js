e8964dcfe53751c8a0462a90af11e26844bec747
Rishat <xpan96@gmail.com>
2018-11-12 22:52:57 +0300

    two params in get

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.get('/contact', function(req, res) {
     res.send('hello');
 });
 
-app.get('/contact/:id', function(req, res) {
-    res.send(`id = ${req.params.id}`);
+app.get('/contact/:name/:id', function(req, res) {
+    res.send(`name = ${req.params.name}, id = ${req.params.id}`);
 });
 
 app.listen(3000)
