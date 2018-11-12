d55d1cd390fd97c87c5e154dc0679dd46c9ec721
Rishat <xpan96@gmail.com>
2018-11-12 22:48:21 +0300

    qet query & param

--- index.js
+++ index.js
@@ -5,4 +5,12 @@ app.get('/', function(req, res) {
     res.send('This is home');
 });
 
+app.get('/contact', function(req, res) {
+    res.send('hello');
+});
+
+app.get('/contact/:id', function(req, res) {
+    res.send(`id = ${req.params.id}`);
+});
+
 app.listen(3000)
