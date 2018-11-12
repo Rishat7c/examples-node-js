d4d031184acd7ab34dcd252d45aa5c9dfc2217b5
Rishat <xpan96@gmail.com>
2018-11-12 21:05:48 +0300

    re-write git commit -m re-write

--- thread_index.js
+++ thread_index.js
@@ -5,7 +5,9 @@ var myReadShort = fs.createReadStream('./text.txt', 'utf8');
 var myWriteShort = fs.createWriteStream('./news.txt');
 
 // Чтение data 
-myReadShort.on('data', function(chunk) {
-    console.log(`Новые данные получены\n${chunk}`);
-    myWriteShort.write(chunk);
-});
+// myReadShort.on('data', function(chunk) {
+//     console.log(`Новые данные получены\n${chunk}`);
+//     myWriteShort.write(chunk);
+// });
+
+myReadShort.pipe(myWriteShort);
