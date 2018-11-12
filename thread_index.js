f9382b7ce0e5ec315364f48f6dad2a85b71aa9d8
Rishat <xpan96@gmail.com>
2018-11-12 22:04:44 +0300

    404 and add links in main index

--- thread_index.js
+++ thread_index.js
@@ -20,6 +20,9 @@ var server = http.createServer(function(req, res) {
         } else if(req.url === '/contact') {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8;'});
             fs.createReadStream('./pages/contact.html').pipe(res);
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8;'});
+            fs.createReadStream('./pages/404.html').pipe(res);
         }
     }
 });
