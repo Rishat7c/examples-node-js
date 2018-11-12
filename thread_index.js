60bcebabe40142541106fd1da90b663f2b103d5e
Rishat <xpan96@gmail.com>
2018-11-12 21:15:30 +0300

    read file and pipe in server

--- thread_index.js
+++ thread_index.js
@@ -1,13 +1,23 @@
 var fs = require('fs');
 
-// Чтение потока & файл
-var myReadShort = fs.createReadStream('./text.txt', 'utf8');
-var myWriteShort = fs.createWriteStream('./news.txt');
-
 // Чтение data 
 // myReadShort.on('data', function(chunk) {
 //     console.log(`Новые данные получены\n${chunk}`);
 //     myWriteShort.write(chunk);
 // });
 
-myReadShort.pipe(myWriteShort);
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    if(req.url != "/favicon.ico")
+        console.log(req.url);
+
+    res.writeHead(200, {'Content-Type': 'text/plain; charset=utf-8'});
+    // Чтение потока & файл
+    var myReadShort = fs.createReadStream('./text.txt', 'utf8');
+    myReadShort.pipe(res);
+
+});
+
+server.listen(3000, 'localhost');
+console.log('*:3000');
