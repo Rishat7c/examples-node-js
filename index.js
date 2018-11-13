e394942cc3ad13469c616003e26d79eb59933c00
Rishat <xpan96@gmail.com>
2018-11-13 18:09:35 +0300

    sendFile

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send('This is home');
+    res.sendFile('./pages/index.html');
 });
 
 app.get('/contact', function(req, res) {
