28386ddd6d49925f4bfb0e4458f8d90da54be408
Rishat <xpan96@gmail.com>
2018-11-13 18:14:18 +0300

    ejs

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    res.sendFile('./pages/index.html');
+    res.sendFile(__dirname + '/pages/index.html');
 });
 
 app.get('/contact', function(req, res) {
-    res.send('hello');
+    res.sendFile(__dirname + '/pages/contact.html');
 });
 
-app.get('/contact/:name/:id', function(req, res) {
+app.get('/contact/:id', function(req, res) {
     res.send(`name = ${req.params.name}, id = ${req.params.id}`);
 });
 
